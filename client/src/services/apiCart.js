817aefceb814aaa5789a5df58b727cbf39c304e2
XIII <minibrusp@gmail.com>
2023-11-18 04:21:03 +0800

    - client adjustments for fetch urls

--- client/src/services/apiCart.js
+++ client/src/services/apiCart.js
@@ -1,22 +1,30 @@
 export const checkoutCart = async (cart) => {
-  const response = await fetch('http://localhost:5050/checkout', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(cart),
-  });
+  // const response = await fetch('http://localhost:5050/checkout', {
+  const response = await fetch(
+    'https://e-commerce-product-page-flame.vercel.app/checkout',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cart),
+    }
+  );
   const json = response.json();
 
   return json;
 };
 
 export const getSessionById = async (id) => {
-  const response = await fetch('http://localhost:5050/retrieve', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ id }),
-  });
+  // const response = await fetch('http://localhost:5050/retrieve', {
+  const response = await fetch(
+    'https://e-commerce-product-page-flame.vercel.app/retrieve',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id }),
+    }
+  );
   const json = await response.json();
 
   return json;
